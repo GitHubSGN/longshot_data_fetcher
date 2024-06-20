b731009b3b8a71f1f5300a98770da4d7c58c5fbe
Jun Chen <junchen@Juns-MacBook-Pro.local>
2024-06-20 14:41:28 +0800

    init submit on arb on perps only

--- backtest_exp/t20240617_ranking_bst.py
+++ backtest_exp/t20240617_ranking_bst.py
@@ -9,9 +9,11 @@ from tools.dir_util import project_dir
 
 warnings.filterwarnings('ignore', category=RuntimeWarning, message='All-NaN slice encountered')
 
+rank_const = 100
+
 def top_percent_columns(row, percent_unit, top_rank):
-    lower_threshold = np.nanpercentile(row, 100 - percent_unit * top_rank)
-    upper_threshold = np.nanpercentile(row, 100 - percent_unit * top_rank + percent_unit)
+    lower_threshold = np.nanpercentile(row, rank_const - percent_unit * top_rank)
+    upper_threshold = np.nanpercentile(row, rank_const - percent_unit * top_rank + percent_unit)
     # print(lower_threshold, upper_threshold)
     idx = (row >= lower_threshold) & (row <= upper_threshold)
     return idx / sum(idx)
@@ -42,31 +44,21 @@ def ranking_strategy_bst(funding_df, return_df, start_shift = 1, factor_horizon=
     df_factor = funding_df.rolling(window=factor_horizon * 3).mean()
 
     pnl_df = None
-    for tr in range(1, int(100/percent_unit)+1):
-        print(f"top{tr}:")
-        signal = df_factor.apply(lambda x: top_percent_columns(x, percent_unit, tr), axis=1)
-        # (signal.shift(1) * df).sum(axis=1).cumsum().plot()
-        resampled_signal = signal.tail(-start_shift).resample(f'{prediction_horizon}d', offset=f'{prediction_horizon}d',
-                                                              label='right', closed='right', origin='start').last()
-        resampled_signal = resampled_signal.reindex(return_df.index).ffill()
+    signal_long = df_factor.apply(lambda x: top_percent_columns(x, percent_unit, 1), axis=1)
+    signal_short = -df_factor.apply(lambda x: top_percent_columns(x, percent_unit, int(rank_const / percent_unit)), axis=1)
+    signal = (signal_long + signal_short)/2.0
+    resampled_signal = signal.tail(-start_shift).resample(f'{prediction_horizon}d', offset=f'{prediction_horizon}d', label='right', closed='right', origin='start').last()
+    resampled_signal = resampled_signal.reindex(return_df.index).ffill()
 
-        turnover = np.abs(resampled_signal.diff()).sum(axis=1)
-        print(f"Avg. turnover per day: {sum(turnover) / ((resampled_signal.index[-1] - resampled_signal.index[0]) / pd.to_timedelta('1d'))}")
-        print(f"Avg. return per trade: {(resampled_signal.shift(1) * return_df).sum(axis=1).sum() / sum(turnover)}")
+    turnover = np.abs(resampled_signal.diff()).sum(axis=1)
+    print(f"Avg. turnover per day: {sum(turnover) / ((resampled_signal.index[-1] - resampled_signal.index[0]) / pd.to_timedelta('1d'))}")
+    print(f"Avg. return per trade: {(resampled_signal.shift(1) * return_df).sum(axis=1).sum() / sum(turnover)}")
 
-        # (resampled_signal.shift(1) * df).sum(axis=1).cumsum().plot()
-        # plt.show()
-        pnl = (resampled_signal.shift(1) * return_df).sum(axis=1)
-        pnl.name = f"top{tr}"
-        pnl_df = pnl.copy() if pnl_df is None else pd.concat([pnl_df, pnl], axis=1)
-        print(f"cum pnl: {pnl.sum()}")
-
-    pnl_df.to_excel(f"ranking_bst_pnl-{start_shift}-{factor_horizon}-{prediction_horizon}-{percent_unit}.xlsx", index=True)
-    pnl_df.index = return_df.index
+    pnl_df = (resampled_signal.shift(1) * return_df).sum(axis=1)
     cum_pnl_df = pnl_df.cumsum()
-    if show:
-        cum_pnl_df.plot()
-        plt.show()
+    cum_pnl_df.plot()
+    plt.show()
+
     return cum_pnl_df
 
 
@@ -101,7 +93,7 @@ def ranking_bst(exchange = "bybit"):
         pnl_spread = np.log(df_spot).diff() - np.log(df_perp).diff()
         return_df = pnl_spread + df
 
-        ranking_strategy_bst(df, return_df, start_shift=3, factor_horizon=7, prediction_horizon=21, percent_unit=10, show=True)
+        ranking_strategy_bst(df, np.log(df_perp).diff(), start_shift=3, factor_horizon=5, prediction_horizon=1, percent_unit=5, show=True)
 
     print("Done")
 
