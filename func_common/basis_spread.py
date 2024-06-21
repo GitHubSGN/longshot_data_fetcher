d51a7d7b9a45173a3e70f91fb23de2cb0fe94b5d
GitHubSGN <free_register@163.com>
2024-06-22 07:01:25 +0800

    cal basis spread using open price

--- func_common/basis_spread.py
+++ func_common/basis_spread.py
@@ -7,13 +7,14 @@ def cal_basis_spread(df_spot: pd.DataFrame, df_perp: pd.DataFrame, symbol_perp: 
     if df_spot is None or df_perp is None:
         return None, None
     else:
+        price_col = "open"
         matches = re.findall(r"100*", symbol_perp.split(f"/")[0])
         if len(matches) > 0:
             multiplier = int(matches[0])
             df_spot[['open', 'high', 'low', 'close']] = df_spot[['open', 'high', 'low', 'close']] * multiplier
             df_spot["volume"] = df_spot["volume"] / multiplier
-        df = pd.merge(df_spot[["open_time", "close"]], df_perp[["open_time", "close"]], on="open_time", how="inner",
+        df = pd.merge(df_spot[["open_time", price_col]], df_perp[["open_time", price_col]], on="open_time", how="inner",
                       suffixes=["Spot", "Perps"])
-        df["spread"] = (df["closeSpot"] - df["closePerps"]) / (df["closePerps"]) * 10000
+        df["spread"] = (df[f"{price_col}Spot"] - df[f"{price_col}Perps"]) / (df[f"{price_col}Perps"]) * 10000
 
         return df["spread"].mean(), df["spread"].values[-1], df["spread"].values[0], df
