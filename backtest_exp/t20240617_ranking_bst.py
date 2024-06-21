e704ca07752308b6b4a394ac0a58b0058ba27a1b
GitHubSGN <free_register@163.com>
2024-06-21 18:23:13 +0800

    1. Consider Transaction Cost 2. Calculate Sharp 3. Add Baseline: BTC/ETH 4. read csv instead of xlsx 5. tranverse xlsx rename 6. various df_factor

--- backtest_exp/t20240617_ranking_bst.py
+++ backtest_exp/t20240617_ranking_bst.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import warnings
 from datetime import datetime
@@ -6,8 +7,12 @@ import numpy as np
 import pandas as pd
 
 from tools.dir_util import project_dir
+from tools.timer_utils import timer
 
 warnings.filterwarnings('ignore', category=RuntimeWarning, message='All-NaN slice encountered')
+
+COMISSION = 4.6 * 2 / 1e4
+SLIPPAGE = 10 / 1e4
 
 def top_percent_columns(row, percent_unit, top_rank):
     lower_threshold = np.nanpercentile(row, 100 - percent_unit * top_rank)
@@ -38,12 +43,22 @@ def deal_data():
     df_perp.to_csv(os.path.join(project_dir(), "data", "a_funding", "bybit", 'df_perp.csv'))
     df_spot.to_csv(os.path.join(project_dir(), "data", "a_funding", "bybit", 'df_spot.csv'))
 
-def ranking_strategy_bst(funding_df, return_df, start_shift = 1, factor_horizon=7, prediction_horizon=21, percent_unit=10, show=True, print_log=True):
+def cal_sharp_ratio(pnl):
+    ret_ap_avg = pnl.mean() * (pd.to_timedelta('365d') / (pnl.index[-1] - pnl.index[-2]))
+    ret_ap_std = pnl.std() * np.sqrt(pd.to_timedelta('365d') / (pnl.index[-1] - pnl.index[-2]))
+    ret_sharp = pnl.mean() / pnl.std() * np.sqrt(pd.to_timedelta('365d') / (pnl.index[-1] - pnl.index[-2]))
+    return ret_ap_avg, ret_ap_std, ret_sharp
+
+def ranking_strategy_bst(funding_df, return_df, start_shift = 1, factor_horizon=7, prediction_horizon=21, percent_unit=10,
+                         top_bunches = 100, show=False, print_log=False, save=False):
     df_factor = funding_df.rolling(window=factor_horizon * 3).mean()
+    # df_factor = funding_df.rolling(window=factor_horizon * 3).mean() - 1 * funding_df.rolling(window=factor_horizon * 3).std()
+    # df_factor = funding_df.rolling(window=factor_horizon * 3).mean() * 3 * 365 - 1 * funding_df.rolling(window=factor_horizon * 3).std() * np.sqrt(3*365)
 
     pnl_df = None
+    tc_df = None
     turnover_list = []
-    for tr in range(1, 2): #int(100/percent_unit)+1):
+    for tr in range(1, min([int(100/percent_unit)+1, top_bunches+1])):
         if print_log:
             print(f"top{tr}:")
         signal = df_factor.apply(lambda x: top_percent_columns(x, percent_unit, tr), axis=1)
@@ -61,24 +76,43 @@ def ranking_strategy_bst(funding_df, return_df, start_shift = 1, factor_horizon=
         # (resampled_signal.shift(1) * df).sum(axis=1).cumsum().plot()
         # plt.show()
         pnl = (resampled_signal.shift(1) * return_df).sum(axis=1)
-        pnl.name = f"top{tr}"
-        pnl_df = pnl.copy() if pnl_df is None else pd.concat([pnl_df, pnl], axis=1)
-        if print_log:
-            print(f"cum pnl: {pnl.sum()}")
+        tc = (resampled_signal.diff().abs() * (COMISSION + SLIPPAGE)).sum(axis=1)
 
-    # pnl_df.to_excel(f"ranking_bst_pnl-{start_shift}-{factor_horizon}-{prediction_horizon}-{percent_unit}.xlsx", index=True)
+        pnl.name = f"top{tr}"
+        pnl_df = pd.concat([pnl_df, pnl], axis=1)
+        tc.name = f"top{tr}"
+        tc_df = pd.concat([tc_df, tc], axis=1)
+        if print_log:
+            print(f"cum pnl: {pnl.sum()}, cum tc: {tc.sum()} => cum return: {pnl.sum() - tc.sum()}")
+
+    # add baseline
+    for token in ["BTC", "ETH"]:
+        pnl = return_df[token]
+        pnl_df = pd.concat([pnl_df, pnl], axis=1)
+        tc = pd.Series(0, index=pnl.index, name=pnl.name)
+        tc_df = pd.concat([tc_df, tc], axis=1)
+
+    if save:
+        pnl_df.to_excel(f"ranking_bst_pnl-{start_shift}-{factor_horizon}-{prediction_horizon}-{percent_unit}.xlsx", index=True)
     pnl_df.index = return_df.index
-    cum_pnl_df = pnl_df.cumsum()
     if show:
-        cum_pnl_df.plot()
+        pnl_df.cumsum().plot(title = "Cum. Funding Rate Ret.")
+        plt.legend(loc='upper left')
         plt.show()
-    return cum_pnl_df, turnover_list
+        (pnl_df.cumsum() - tc_df.cumsum()).plot(title="Cum. Funding Rate Ret. (with TC)")
+        plt.legend(loc='upper left')
+        plt.show()
+    return pnl_df, tc_df, turnover_list
 
 
 def ranking_bst(exchange = "bybit"):
     if exchange == "bybit":
-        xlsx_fn = os.path.join(project_dir(), "data", "a_funding", exchange, "FundingRate-90tokens_2023-01-01_2024-06-17.xlsx")
-        df = pd.read_excel(xlsx_fn, index_col=0)
+        # with timer("read xlsx"):
+        #     xlsx_fn = os.path.join(project_dir(), "data", "a_funding", exchange, "FundingRate-90tokens_2023-01-01_2024-06-17.xlsx")
+        #     df = pd.read_excel(xlsx_fn, index_col=0)
+        with timer("read csv"):
+            csv_fn = os.path.join(project_dir(), "data", "a_funding", exchange, "FundingRate-90tokens_2023-01-01_2024-06-17.csv")
+            df = pd.read_csv(csv_fn, index_col=0)
         # df.fillna(0, inplace=True)
         df.index = pd.DatetimeIndex(df.index)
 
@@ -106,14 +140,29 @@ def ranking_bst(exchange = "bybit"):
         pnl_spread = np.log(df_spot).diff() - np.log(df_perp).diff()
         return_df = pnl_spread + df
 
+        case_study = False
+        if case_study:
+            pnl_df, tc_df, turnover_list = ranking_strategy_bst(df, df, start_shift=3, factor_horizon=7, prediction_horizon=21, percent_unit=10, top_bunches=10, show=True, print_log=True)
+            pnl_df, tc_df, turnover_list = ranking_strategy_bst(df, df, start_shift=3, factor_horizon=7, prediction_horizon=1, percent_unit=10, top_bunches=10, show=True, print_log=True)
+            ret_ap_avg, ret_ap_std, ret_sharp = cal_sharp_ratio(pnl_df)
+            display_cols = [f"top{i}" for i in range(1,4)] + ["BTC", "ETH"]
+            for col in display_cols:
+                ravg, rstd, rshp = ret_ap_avg[col], ret_ap_std[col], ret_sharp[col]
+                print(f"[W.O. TC, {col}] Avg. Ret(a.p.): {ravg:.2%}, Std. Ret(a.p.): {rstd:.2%}, Sharp Ratio: {rshp:.2f}")
+            ret_ap_avg, ret_ap_std, ret_sharp = cal_sharp_ratio(pnl_df - tc_df)
+            for col in display_cols:
+                ravg, rstd, rshp = ret_ap_avg[col], ret_ap_std[col], ret_sharp[col]
+                print(f"[With TC, {col}] Avg. Ret(a.p.): {ravg:.2%}, Std. Ret(a.p.): {rstd:.2%}, Sharp Ratio: {rshp:.2f}")
+
         res = []
+        xlsx_pos = datetime.now().strftime('%y%m%d-%H%M%S')
         for fh in range(1, 31):
             for ph in range(1, 31):
-                cum_pnl_df, turnover_list = ranking_strategy_bst(df, df, start_shift=3, factor_horizon=fh, prediction_horizon=ph, percent_unit=10, show=False, print_log=False)
-                res.append( [fh, ph, cum_pnl_df.iloc[-1], turnover_list[0]] )
-                print(fh, ph, cum_pnl_df.iloc[-1], turnover_list[0])
+                pnl_df, tc_df, turnover_list = ranking_strategy_bst(df, df, start_shift=3, factor_horizon=fh, prediction_horizon=ph, percent_unit=5, top_bunches=1, show=False, print_log=False)
+                res.append( [fh, ph, pnl_df.sum(), turnover_list[0]] )
+                print(fh, ph, pnl_df.sum(), turnover_list[0])
                 res_df = pd.DataFrame(res, columns=["factor_horizon", "prediction_horizon", "cum_pnl", "turnover"])
-                res_df.to_excel("tranverse.xlsx")
+                res_df.to_excel(f"tranverse_{xlsx_pos}.xlsx")
 
     print("Done")
 
